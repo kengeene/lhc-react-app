7d610d77fd84246dfa542fabc4e6e7c88801a085
enkuria <kengeene@gmail.com>
2024-06-19 09:04:42 +0300

    feat: add nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import * as React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
+import { NextUIProvider } from "@nextui-org/react";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <NextUIProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </NextUIProvider>
   );
 }
